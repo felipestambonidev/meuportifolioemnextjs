dba46f4d2c183ec776caa4c2cc5a21a72c4b0a11
felipestambonidev <felipestambonii@gmail.com>
2024-08-14 17:50:05 -0300

    edit: editei a fonte que estou usando, estou usando Work Sans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,23 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.tsx
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
