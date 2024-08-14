fb13c9d7d9bbf2245d045ead54a61b05193d5d21
felipestambonidev <felipestambonii@gmail.com>
2024-08-14 18:06:22 -0300

    Editei o select para funcionar o idioma em ingles

--- components/menucabecalhoenglish/index.tsx
+++ components/menucabecalhoenglish/index.tsx
@@ -17,42 +17,44 @@ const NavbarEng: React.FC = () => {
   return (
     <nav className="p-8 flex justify-between items-center relative">
       <div className="flex-grow"></div>
+
+      {/* Menu Principal (Desktop) */}
       <ul className="hidden md:flex items-center space-x-10">
         <li><Link href="#" className="text-white text-xl font-light hover:text-orange-500">About Me</Link></li>
         <li><Link href="#" className="text-white text-xl font-light hover:text-orange-500">Technologies</Link></li>
         <li><Link href="#" className="text-white text-xl font-light hover:text-orange-500">My Projects</Link></li>
         <li><Link href="#" className="text-white text-xl font-light hover:text-orange-500">Get in Touch</Link></li>
       </ul>
-      <div className="flex items-center">
-        <div className="relative flex items-center ml-4">
-          <div onClick={toggleDropdown} className="flex items-center cursor-pointer">
-            <Image
-              src="/img/icons/eua.png"
-              alt="Language selector"
-              width={15}
-              height={15}
-            />
-            <span className={`ml-2 transform transition-transform text-white text-[10px] ${isDropdownOpen ? 'rotate-180' : ''}`}>
-              ▼
-            </span>
-          </div>
-          {isDropdownOpen && (
-            <div className="absolute top-6 right-0 bg-[#474747] shadow-lg rounded">
-              <ul className="text-black">
-                <li className="p-3 hover:bg-orange-500 text-white font-medium cursor-pointer" onClick={() => window.location.href = '/'}>Português</li>
-                <li className="p-3 hover:bg-orange-500 text-white font-medium cursor-pointer" onClick={() => window.location.href = '/en'}>English</li>
-              </ul>
-            </div>
-          )}
-        </div>
 
-        {/* Ícone de Menu Hamburguer */}
-        <div className="ml-4 md:hidden" onClick={toggleMenu}>
-          <div className="space-y-2 cursor-pointer">
-            <span className="block w-8 h-0.5 bg-white"></span>
-            <span className="block w-8 h-0.5 bg-white"></span>
-            <span className="block w-8 h-0.5 bg-white"></span>
+      {/* Seletor de Idioma */}
+      <div className="relative flex items-center ml-4 z-20">
+        <div onClick={toggleDropdown} className="flex items-center cursor-pointer">
+          <Image
+            src="/img/icons/eua.png"
+            alt="Language selector"
+            width={15}
+            height={15}
+          />
+          <span className={`ml-2 transform transition-transform text-white text-[10px] ${isDropdownOpen ? 'rotate-180' : ''}`}>
+            ▼
+          </span>
+        </div>
+        {isDropdownOpen && (
+          <div className="absolute top-6 right-0 bg-[#474747] shadow-lg rounded">
+            <ul className="text-white">
+              <li className="p-3 hover:bg-orange-500 font-medium cursor-pointer" onClick={() => window.location.href = '/'}>Portuguese</li>
+              <li className="p-3 hover:bg-orange-500 font-medium cursor-pointer" onClick={() => window.location.href = '/en'}>English</li>
+            </ul>
           </div>
+        )}
+      </div>
+
+      {/* Ícone de Menu Hamburguer */}
+      <div className="ml-4 md:hidden z-10" onClick={toggleMenu}>
+        <div className="space-y-2 cursor-pointer">
+          <span className="block w-8 h-0.5 bg-white"></span>
+          <span className="block w-8 h-0.5 bg-white"></span>
+          <span className="block w-8 h-0.5 bg-white"></span>
         </div>
       </div>
 
