ff571bced54dc2ecc7a06f9aba98c9b22f4e20dc
felipestambonidev <felipestambonii@gmail.com>
2024-08-14 17:47:40 -0300

    add: adicionei as resoluções para a responsividade

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    "./data/*.{json}"],
   theme: {
     extend: {
       backgroundImage: {
@@ -13,9 +13,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-      },
+    },
+    screens: {
+      'xs': '0px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
