d71d975a29427f456a70299ad959f58434dd50fa
felipestambonidev <felipestambonii@gmail.com>
2024-08-14 17:51:05 -0300

    Add: adicionei a página principal em inglês

---
+++ pages/en/index.tsx
@@ -0,0 +1,17 @@
+import NavbarEng from "@/components/menucabecalhoenglish"
+import MenuLateralEng from "@/components/menulateralenglish"
+
+export default function HomeEn(){
+	return(
+		<div>
+			<section>
+				<NavbarEng />
+			</section>
+			<section>
+				<MenuLateralEng />
+			</section>
+		</div>
+	)
+}
+
+	
