1217e550ccc98230dfdc3d7dbeb48c2a20d0e2aa
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 15:28:56 -0300

    Edit: ajustei de novo a diagramação

--- components/sobremim/index.tsx
+++ components/sobremim/index.tsx
@@ -1,9 +1,9 @@
 export default function SobreMim() {
   return (
     <div className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white">
-      <h1 className="lg:text-4xl xs:text-3xl md:text-start xs:text-center">Sobre Mim</h1>
+      <h1 className="lg:text-4xl xs:text-3xl">Sobre Mim</h1>
       <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
-      <p className="lg:text-xl xs:text-lg xs:text-center md:text-start font-light">
+      <p className="lg:text-xl xs:text-[17px] font-light">
         Olá, sou um desenvolvedor front-end e posso construir seu site como você
         deseja. Sou proativo, sempre buscando soluções eficientes para qualquer
         desafio que surgir. Tenho um perfil educado e gosto de manter uma
--- components/sobremimenglish/index.tsx
+++ components/sobremimenglish/index.tsx
@@ -1,9 +1,9 @@
 export default function SobreMimEng() {
   return (
     <div className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white">
-      <h1 className="lg:text-4xl xs:text-3xl md:text-start xs:text-center">About Me</h1>
+      <h1 className="lg:text-4xl xs:text-3xl">About Me</h1>
       <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
-      <p className="lg:text-xl xs:text-lg xs:text-center md:text-start font-light">
+      <p className="lg:text-xl xs:text-[17px]">
         Hello, I am a front-end developer and I can build your website as you
         wish. I am proactive, always looking for efficient solutions to any
         challenge that may arise. I have a polite profile and I like to maintain
