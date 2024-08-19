b8921776bb96b95ab6df6f24f9c8232eb38f4b36
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 19:20:15 -0300

    removi o caminho errado

--- pages/en/index.tsx
+++ pages/en/index.tsx
@@ -3,7 +3,6 @@ import ProjetosWordPress from "@/components/meusprojetos/WordPress";
 import MeusProjetosEng from "@/components/meusprojetosenglish";
 import PerfilEng from "@/components/perfilenglish";
 import RodapeEng from "@/components/rodapeenglish";
-import Rodape from "@/components/rodapeenglish";
 import SobreMimEng from "@/components/sobremimenglish";
 import TecnologiasEng from "@/components/tecnologiasenglish";
 
