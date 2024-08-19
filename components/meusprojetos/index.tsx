d92d767954823d506c7a68acbb379184829a6add
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 18:40:51 -0300

    add: adicionei a parte dos meus projetos em pt e ingles

---
+++ components/meusprojetos/index.tsx
@@ -0,0 +1,15 @@
+export default function MeusProjetos() {
+  return (
+    <div
+      className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white"
+      id="projetos"
+    >
+      <h1 className="lg:text-4xl xs:text-3xl">Meus Projetos</h1>
+      <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
+      <p className="lg:text-xl xs:text-[17px] font-light">Em Breve</p>
+      <p className="lg:text-xl xs:text-[17px] font-light">Em Breve</p>
+      <p className="lg:text-xl xs:text-[17px] font-light">Em Breve</p>
+      <p className="lg:text-xl xs:text-[17px] font-light">Em Breve</p>
+    </div>
+  );
+}
