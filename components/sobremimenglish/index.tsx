afeb5a7a70db955759aad2a3f70245db99a736d9
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 15:13:33 -0300

    Add: Fiz o componente sobre mim em inglês

---
+++ components/sobremimenglish/index.tsx
@@ -0,0 +1,16 @@
+export default function SobreMimEng() {
+  return (
+    <div className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white">
+      <h1 className="lg:text-4xl xs:text-3xl">About Me</h1>
+      <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
+      <p className="lg:text-xl xs:text-lg font-light">
+        Hello, I am a front-end developer and I can build your website as you
+        wish. I am proactive, always looking for efficient solutions to any
+        challenge that may arise. I have a polite profile and I like to maintain
+        clear and respectful communication with my clients and colleagues. I am
+        constantly learning new technologies and I am ready to adapt to the
+        needs of your project, always ensuring the best results.
+      </p>
+    </div>
+  );
+}
