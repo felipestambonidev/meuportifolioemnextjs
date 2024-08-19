bf01a0517e30308aeb10579c2fe0bdfbdd3dda16
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 15:13:08 -0300

    Edit: Coloquei o conteudo do sobre mim

--- components/sobremim/index.tsx
+++ components/sobremim/index.tsx
@@ -1,7 +1,17 @@
-export default function SobreMim(){
+export default function SobreMim() {
   return (
-    <div className="bg-[#2d2d2d] p-5 xs:mx-8 lg:mx-52 w-auto mt-20 text-white">
-        <h1 className="text-2xl">Sobre Mim</h1>
+    <div className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white">
+      <h1 className="lg:text-4xl xs:text-3xl">Sobre Mim</h1>
+      <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
+      <p className="lg:text-xl xs:text-lg font-light">
+        Olá, sou um desenvolvedor front-end e posso construir seu site como você
+        deseja. Sou proativo, sempre buscando soluções eficientes para qualquer
+        desafio que surgir. Tenho um perfil educado e gosto de manter uma
+        comunicação clara e respeitosa com meus clientes e colegas. Estou
+        constantemente aprendendo novas tecnologias e estou pronto para me
+        adaptar às necessidades do seu projeto, garantindo sempre os melhores
+        resultados.
+      </p>
     </div>
   );
 }
