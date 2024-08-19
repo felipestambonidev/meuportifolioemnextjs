04eb4ac21a52e40da6c6f0abcd7d1866cd5696b8
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 14:56:04 -0300

    Add: Adicionei o sobre mim

---
+++ components/sobremim/index.tsx
@@ -0,0 +1,7 @@
+export default function SobreMim(){
+  return (
+    <div className="bg-[#2d2d2d] p-5 xs:mx-8 lg:mx-52 w-auto mt-20 text-white">
+        <h1 className="text-2xl">Sobre Mim</h1>
+    </div>
+  );
+}
