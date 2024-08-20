8df7dfc13b2fe7af52923641d7c4ee550eeb5054
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 22:49:30 -0300

    edit: reafatorei o sobre mim

--- components/sobremim/index.tsx
+++ components/sobremim/index.tsx
@@ -1,17 +1,17 @@
+import sobremim from "@/data/sobremim/sobremim.json";
+
 export default function SobreMim() {
   return (
-    <div className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white" id="sobre-mim">
-      <h1 className="lg:text-4xl xs:text-3xl">Sobre Mim</h1>
-      <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
-      <p className="lg:text-xl xs:text-[17px] font-light">
-        Olá, sou um desenvolvedor front-end e posso construir seu site como você
-        deseja. Sou proativo, sempre buscando soluções eficientes para qualquer
-        desafio que surgir. Tenho um perfil educado e gosto de manter uma
-        comunicação clara e respeitosa com meus clientes e colegas. Estou
-        constantemente aprendendo novas tecnologias e estou pronto para me
-        adaptar às necessidades do seu projeto, garantindo sempre os melhores
-        resultados.
-      </p>
+    <div
+      className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white"
+      id="sobre-mim">
+      {sobremim.map((sobremimfe, index) => (
+        <div key={index}>
+          <h1 className="lg:text-4xl xs:text-3xl">{sobremimfe.title}</h1>
+          <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
+          <p className="lg:text-xl xs:text-[17px] font-light">{sobremimfe.paragraph}</p>
+        </div>
+      ))}
     </div>
   );
 }
