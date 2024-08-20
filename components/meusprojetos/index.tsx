fdc9a42085f0b1ea84fe162c73515739cfd9033b
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 21:44:01 -0300

    Ajustes

--- components/meusprojetos/index.tsx
+++ components/meusprojetos/index.tsx
@@ -2,7 +2,7 @@ export default function MeusProjetos() {
   return (
     <div
       className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white"
-      id="projetos"
+      id="meus-projetos"
     >
       <h1 className="lg:text-4xl xs:text-3xl">Meus Projetos</h1>
       <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
