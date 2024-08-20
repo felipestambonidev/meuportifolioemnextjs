d376e112067a155b3a4fc9792f1025bb88123154
felipestambonidev <felipestambonii@gmail.com>
2024-08-19 21:18:53 -0300

    Add: Fiz os formulários em Inglês e Português

---
+++ components/contato/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import emailjs from '@emailjs/browser'
+
+export default function Contato() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  function sendEmail(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+
+    if(name === '' || email === '' || message === ''){
+        alert("Preencha todos os campos")
+        return;
+    }
+
+
+    const templateParams = {
+        from_name: name,
+        message: message,
+        email: email
+    }
+
+    emailjs.send("service_6g6c1pc", "template_91o3mq6", templateParams, "zjZ6JxujDEXl7Gatg")
+    .then((response) => {
+        console.log('EMAIL ENVIADO', response.status, response.text)
+        setName('')
+        setEmail('')
+        setMessage('')
+    }, (error) => {
+        console.log("ERRO: ", error)
+    })
+  }
+
+  return (
+    <div className="bg-[#1E1E1E] drop-shadow-lg p-5 xs:mx-8 lg:mx-52 w-auto mt-20 rounded-lg text-white">
+      <h1 className="lg:text-4xl xs:text-3xl">Entre em Contato</h1>
+      <span className="block w-full h-1 bg-[#2E2D2D] my-4" />
+      <form className="flex flex-col space-y-4" onSubmit={sendEmail}>
+        <input
+          className="p-3 bg-[#2E2D2D] rounded-md text-white focus:outline-none focus:ring-2 focus:ring-[#4A4A4A]"
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          className="p-3 bg-[#2E2D2D] rounded-md text-white focus:outline-none focus:ring-2 focus:ring-[#4A4A4A]"
+          type="text"
+          placeholder="Digite seu e-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <textarea 
+          className="p-3 bg-[#2E2D2D] rounded-md text-white focus:outline-none focus:ring-2 focus:ring-[#4A4A4A]"
+          placeholder="Digite sua mensagem"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <input
+          type="submit"
+          value="Enviar"
+          className="p-3 bg-[#4A4A4A] rounded-md text-white cursor-pointer hover:bg-orange-500 transition-all"
+        />
+      </form>
+    </div>
+  );
+  
+}
